44f9dd897299101837d3093a507d52b65922e0b0
jiajunliu0024 <jiajunliu0024@gmail.com>
2024-07-31 19:23:28 +1000

    solve the code check warning

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Home from "./page";
 import { LoadScript } from "@react-google-maps/api";
-import { APIProvider } from "@vis.gl/react-google-maps";
 
 function App() {
   const libraries = ["places"];
-  const apiKey = "AIzaSyA6d7pIq9TszfM0M6pIosMT1flSKr5o8oM";
-
+  const apiKey = process.env.GOOGLE_MAPS_API_KEY;
   return (
     <LoadScript googleMapsApiKey={apiKey} libraries={libraries}>
       <div className="App">
