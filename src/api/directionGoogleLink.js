077759c0f164918b4b1c0dbcec238e723cae7f3a
jiajunliu0024 <jiajunliu0024@gmail.copm>
2024-08-13 20:49:42 +1000

    correct the direction link

--- src/api/directionGoogleLink.js
+++ src/api/directionGoogleLink.js
@@ -2,7 +2,8 @@ import React from "react";
 const getDirectionLink = (infoStation) => {
   let waypoints = null;
   if (infoStation) {
-    waypoints = infoStation.location_x + "," + infoStation.location_y;
+    waypoints =
+      infoStation.address + " " + infoStation.suburb + " " + infoStation.state;
   }
   const travelMode = "driving";
   const directionsUrl = `https://www.google.com/maps/dir/?api=1&origin=Current+Location&destination=${waypoints}&travelmode=${travelMode}`;
