2b23025f296bc3ad2d0d983d515dc3da40659a24
jiajunliu0024 <jiajunliu0024@gmail.com>
2024-08-15 17:02:03 +1000

    try http first

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 // Create an Axios instance with default configuration
 const axiosInstance = axios.create({
-  baseURL: "https://13.211.62.189:443", // Base URL of your API
+  baseURL: "http://13.211.62.189", // Base URL of your API
   timeout: 30000, // Optional: Set a timeout for requests
+
   headers: {
     "Content-Type": "application/json",
   },
