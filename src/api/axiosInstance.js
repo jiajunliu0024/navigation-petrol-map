0b7db03392ada657b83b68f3582f86c3698b326c
jiajunliu0024 <jiajunliu0024@gmail.copm>
2024-08-15 15:59:35 +1000

    update the port for 443 of https connection

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance with default configuration
 const axiosInstance = axios.create({
-  baseURL: "https://13.211.62.189:8000", // Base URL of your API
+  baseURL: "https://13.211.62.189:443", // Base URL of your API
   timeout: 30000, // Optional: Set a timeout for requests
   headers: {
     "Content-Type": "application/json",
