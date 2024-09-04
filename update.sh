ef6241d64c6eff19e8682570f4115af21fef943b
jiajunliu0024 <jiajunliu0024@gmail.com>
2024-09-04 10:55:45 +1000

    update env and add the correct shell for github

---
+++ update.sh
@@ -0,0 +1,16 @@
+git filter-branch --env-filter
+WRONG_EMAIL="jiajunliu0024@gmail.copm"
+NEW_NAME="jiajunliu0024"
+NEW_EMAIL="jiajunliu0024@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$NEW_NAME"
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
